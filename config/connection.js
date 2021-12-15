def3bd4e3f7f46df637dc8af74c157135dfcee85
Tommy La <tommyl.dmd@gmail.com>
2021-12-14 16:38:20 -0800

    initial commit

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,15 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: '127.0.0.1',
+      username: 'root',
+      password: 'password',
+      database: 'ecommerce_db',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+
 
 module.exports = sequelize;
